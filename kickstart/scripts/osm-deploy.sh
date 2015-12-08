2fe84c63e5647810130371eb5cd347937a39a3b3
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-08 16:33:39 -0700

    be noninteractive

--- kickstart/scripts/osm-deploy.sh
+++ kickstart/scripts/osm-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 deploy_osm_ubuntu() {
-  apt-get install software-properties-common
-  add-apt-repository ppa:kakrueger/openstreetmap
+  apt-get install software-properties-common -y
+  add-apt-repository ppa:kakrueger/openstreetmap -y
   apt-get update
-  apt-get install mapnik osm2pgsql osmosis osmpbf python-gdal geotiff-bin ttf-baekmuk
+  apt-get install mapnik osm2pgsql osmosis osmpbf python-gdal geotiff-bin ttf-baekmuk -y
   #apt-get install libapache2-mod-tile apache2-utils
 }
+
+deploy osm
--- kickstart/scripts/postgis-deploy.sh
+++ kickstart/scripts/postgis-deploy.sh
@@ -4,7 +4,7 @@ deploy_postgis_ubuntu() {
   apt-get install python-software-properties -y
   add-apt-repository ppa:ubuntugis/ppa -y
   apt-get update
-  apt-get install -y postgis
+  apt-get install postgis -y
 }
 
 deploy postgis
