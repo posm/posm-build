7e0b617e8ca074be87bf4beefb7cd1db39395804
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-09 15:33:20 -0700

    Make sure nodesource repo is used (run apt-get update)

--- kickstart/scripts/nodejs-deploy.sh
+++ kickstart/scripts/nodejs-deploy.sh
@@ -3,6 +3,7 @@
 deploy_nodejs_ubuntu() {
   wget -q -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
   echo -e 'deb https://deb.nodesource.com/node_4.x trusty main\ndeb-src https://deb.nodesource.com/node_4.x trusty main' > /etc/apt/sources.list.d/nodesource.list
+  apt-get update
   apt-get install nodejs -y
 }
 
