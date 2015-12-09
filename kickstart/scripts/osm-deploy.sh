e6805ccb6100a2dc841ae338cabf6e487324040c
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-08 17:17:56 -0700

    trim/fix osm packages

--- kickstart/scripts/osm-deploy.sh
+++ kickstart/scripts/osm-deploy.sh
@@ -4,8 +4,13 @@ deploy_osm_ubuntu() {
   apt-get install software-properties-common -y
   add-apt-repository ppa:kakrueger/openstreetmap -y
   apt-get update
-  apt-get install mapnik osm2pgsql osmosis osmpbf python-gdal geotiff-bin ttf-baekmuk -y
-  #apt-get install libapache2-mod-tile apache2-utils
+  apt-get install -y \
+    osm2pgsql \
+    osmosis \
+    osmpbf-bin libosmpbf-dev \
+    python-gdal \
+    geotiff-bin \
+    ttf-baekmuk
 }
 
 deploy osm
