8fdc3b2175bf7173d7201b288834440b77b772c7
Nicholas Hallahan <nick@theoutpost.io>
2015-12-10 15:21:48 -0800

    OMK: copy example settings file to be settings file

--- kickstart/scripts/omk-deploy.sh
+++ kickstart/scripts/omk-deploy.sh
@@ -28,6 +28,7 @@ EOF
 deploy_omk_server() {
 	# install OMK Server
   from_github "https://github.com/AmericanRedCross/OpenMapKitServer" "$dst/OpenMapKitServer"
+  cp $dst/OpenMapKitServer/settings.js.example $dst/OpenMapKitServer/settings.js
   chown -R omk:omk "$dst/OpenMapKitServer"
 
   su - omk -c "cd \"$dst/OpenMapKitServer\" && npm install"
