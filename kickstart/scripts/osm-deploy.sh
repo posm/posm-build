b9771bb22b2324f58356d514b2660b9316b28bf3
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-21 10:13:15 -0700

    Give up on osmosis backport to Trusty for now

--- kickstart/scripts/osm-deploy.sh
+++ kickstart/scripts/osm-deploy.sh
@@ -5,14 +5,29 @@ deploy_osm_ubuntu() {
   add-apt-repository ppa:kakrueger/openstreetmap -y
   apt-get update
   apt-get install -y \
+    osmosis \
     osmpbf-bin libosmpbf-dev \
     python-gdal \
     geotiff-bin \
     ttf-baekmuk
 
   ubuntu_backport_install osmctools
-  ubuntu_backport_install osmosis
   ubuntu_backport_install osm2pgsql
+
+  #backport_osmosis
+}
+
+backport_osmosis() {
+  # extra build deps for osmosis
+  apt-get install -y \
+    default-jdk ivy junit4 ant-optional maven-repo-helper \
+    libbatik-java libcommons-codec-java libcommons-compress-java libcommons-dbcp-java libjpf-java libmysql-java libpostgis-java libpostgresql-jdbc-java libspring-beans-java libspring-context-java libspring-jdbc-java libspring-transaction-java libstax2-api-java libosmpbf-java libplexus-classworlds-java libprotobuf-java libwoodstox-java libxz-java    
+  ubuntu_backport_install netty-3.9
+
+  ubuntu_backport_install gradle-debian-helper # this fails
+
+  # finally backport osmosis itself
+  ubuntu_backport_install osmosis
 }
 
 deploy osm
