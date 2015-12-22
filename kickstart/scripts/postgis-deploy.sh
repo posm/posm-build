8261364fbe2d6c172df12464eaa1b6e427c5ead2
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-22 16:18:11 -0700

    Install other packages

--- kickstart/scripts/postgis-deploy.sh
+++ kickstart/scripts/postgis-deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+pgsql_ver="${pgsql_ver:-9.3}"
+postgis_ver="${postgis_ver:-2.1}"
+
 deploy_postgis_ubuntu() {
   apt-get install software-properties-common -y
   add-apt-repository ppa:ubuntugis/ppa -y
   apt-get update
-  apt-get install postgis -y
+  apt-get install -y \
+    postgis \
+    "postgresql-$pgsql_ver-postgis-$postgis_ver" \
+    "postgresql-$pgsql_ver-postgis-scripts"
 }
 
 deploy postgis
