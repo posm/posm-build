124bcf4931c70c6a310a2ae1f670d779f0ddec57
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-28 12:25:34 -0700

    Initial nginx reverse proxy config [AmericanRedCross/posm#13]

--- kickstart/scripts/nginx-deploy.sh
+++ kickstart/scripts/nginx-deploy.sh
@@ -2,6 +2,10 @@
 
 deploy_nginx_ubuntu() {
   apt-get install nginx -y
+  expand etc/nginx-posm.conf /etc/nginx/sites-available/posm
+  rm /etc/nginx/sites-enabled/default
+  ln -s -f ../sites-available/posm /etc/nginx/sites-enabled/
+  service nginx restart
 }
 
 deploy nginx
