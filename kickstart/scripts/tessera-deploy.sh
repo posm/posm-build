10662f3dce57922e2cf4c35cb0b08bed7fc1b9af
James E. Flemer <james.flemer@ndpgroup.com>
2015-12-28 14:53:49 -0700

    Try to fix mapnik for tessera

--- kickstart/scripts/tessera-deploy.sh
+++ kickstart/scripts/tessera-deploy.sh
@@ -8,10 +8,8 @@ deploy_tessera_ubuntu() {
 
   npm install -g tessera
   local prefix=`npm prefix -g`
+  (cd $prefix/lib/node_modules/tessera && npm install mapnik)
   (cd $prefix/lib/node_modules/tessera && npm install mbtiles tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource tilelive-file tilelive-http tilelive-mapbox)
-
-  # WTF - node-mapnik why do you not install?
-  rsync -av --delete-after $prefix/lib/node_modules/tessera/node_modules/abaculus/node_modules/mapnik/ $prefix/lib/node_modules/tessera/node_modules/tilelive-mapnik/node_modules/mapnik/
 
   # configure
   expand etc/tessera.conf /etc/tessera.conf.json
