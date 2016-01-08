025d157c35f9bf105aef52f426c945ad36afc26b
James E. Flemer <james.flemer@ndpgroup.com>
2016-01-08 12:01:18 -0700

    Install kernel

--- kickstart/scripts/wifi-deploy.sh
+++ kickstart/scripts/wifi-deploy.sh
@@ -2,6 +2,8 @@
 
 # Should use Ubuntu linux-image-3.19.0-42-generic
 deploy_wifi_ubuntu() {
+	apt-get install -y linux-image-3.19.0-42-generic
+
 	ln -s /lib/firmware/iwlwifi-7265D-12.ucode /lib/firmware/iwlwifi-3165-9.ucode
 	ln -s /lib/firmware/iwlwifi-7265-12.ucode /lib/firmware/iwlwifi-3165-12.ucode
 	apt-get install -y hostapd
