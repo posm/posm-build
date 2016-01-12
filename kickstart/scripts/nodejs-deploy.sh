ec4df6c35256f0f3910dc93fbae92cb181becb79
James E. Flemer <james.flemer@ndpgroup.com>
2016-01-11 17:30:53 -0700

    Upgrade to NodeJS v5

--- kickstart/scripts/nodejs-deploy.sh
+++ kickstart/scripts/nodejs-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+node_ver="${node_ver:-5}"
+
 deploy_nodejs_ubuntu() {
   wget -q -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-  echo -e 'deb https://deb.nodesource.com/node_4.x trusty main\ndeb-src https://deb.nodesource.com/node_4.x trusty main' > /etc/apt/sources.list.d/nodesource.list
+  echo -e "deb https://deb.nodesource.com/node_${node_ver}.x trusty main\ndeb-src https://deb.nodesource.com/node_${node_ver}.x trusty main" > /etc/apt/sources.list.d/nodesource.list
   apt-get update
   apt-get install nodejs -y
 
