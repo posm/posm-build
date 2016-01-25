59d739c08c37a16d1ec6f186776514e9f1e7a795
James E. Flemer <james.flemer@ndpgroup.com>
2016-01-25 12:38:14 -0700

    Implement captive portal via DNS wildcard and nginx vhost redirect

--- kickstart/scripts/nginx-deploy.sh
+++ kickstart/scripts/nginx-deploy.sh
@@ -3,8 +3,10 @@
 deploy_nginx_ubuntu() {
   apt-get install nginx -y
   expand etc/nginx-posm.conf /etc/nginx/sites-available/posm
+  expand etc/nginx-captive.conf /etc/nginx/sites-available/captive
   rm /etc/nginx/sites-enabled/default
   ln -s -f ../sites-available/posm /etc/nginx/sites-enabled/
+  ln -s -f ../sites-available/captive /etc/nginx/sites-enabled/
   service nginx restart
 
   mkdir -p /opt/posm-www
