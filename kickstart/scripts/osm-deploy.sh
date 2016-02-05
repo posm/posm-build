9c7da95df55fd03bae51109e45b0a10df3ba41ed
Seth Fitzsimmons <seth@mojodna.net>
2016-02-04 18:27:58 -0800

    Whitespace

--- kickstart/scripts/osm-deploy.sh
+++ kickstart/scripts/osm-deploy.sh
@@ -122,7 +122,7 @@ backport_osmosis() {
   # extra build deps for osmosis
   apt-get install -y \
     default-jdk ivy junit4 ant-optional maven-repo-helper \
-    libbatik-java libcommons-codec-java libcommons-compress-java libcommons-dbcp-java libjpf-java libmysql-java libpostgis-java libpostgresql-jdbc-java libspring-beans-java libspring-context-java libspring-jdbc-java libspring-transaction-java libstax2-api-java libosmpbf-java libplexus-classworlds-java libprotobuf-java libwoodstox-java libxz-java    
+    libbatik-java libcommons-codec-java libcommons-compress-java libcommons-dbcp-java libjpf-java libmysql-java libpostgis-java libpostgresql-jdbc-java libspring-beans-java libspring-context-java libspring-jdbc-java libspring-transaction-java libstax2-api-java libosmpbf-java libplexus-classworlds-java libprotobuf-java libwoodstox-java libxz-java
   ubuntu_backport_install netty-3.9
 
   ubuntu_backport_install gradle-debian-helper # this fails
