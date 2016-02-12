79f00dac942787518b80faae3bd6cc2e3197ec7e
Seth Fitzsimmons <seth@mojodna.net>
2016-02-11 17:13:52 -0700

    Pin npm to ~3.3.12
    Refs AmericanRedCross/posm-build#69

--- kickstart/scripts/nodejs-deploy.sh
+++ kickstart/scripts/nodejs-deploy.sh
@@ -7,6 +7,8 @@ deploy_nodejs_ubuntu() {
   echo -e "deb https://deb.nodesource.com/node_${node_ver}.x trusty main\ndeb-src https://deb.nodesource.com/node_${node_ver}.x trusty main" > /etc/apt/sources.list.d/nodesource.list
   apt-get update
   apt-get install nodejs -y
+  
+  npm install -g npm@~3.3.12
 
   npm install -g interp
 }
