67f4df0e707a9a5871154628628277ad36c30a5a
Seth Fitzsimmons <seth@mojodna.net>
2016-02-16 09:16:34 -0800

    Move mapnik up a level
    Fixes AmericanRedCross/posm#67

--- kickstart/scripts/mbtiles-deploy.sh
+++ kickstart/scripts/mbtiles-deploy.sh
@@ -5,7 +5,12 @@ deploy_mbtiles_ubuntu() {
   apt-get install sqlite3 -y
   npm install -g tl
   local prefix=`npm prefix -g`
+
+  # move mapnik up a level in the tree (virtual tree w/ npm@3, but still...)
+  (cd $prefix/lib/node_modules/tl && find node_modules -type d -name mapnik -exec rm -rf {} \;)
   (cd $prefix/lib/node_modules/tl && npm install mapnik)
+
+  # install useful modules for tile rendering
   (cd $prefix/lib/node_modules/tl && npm install mbtiles tilelive-http tilejson tilelive-mapnik)
 }
 
