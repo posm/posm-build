a91aad768b570dec91efb6ee3d9257e3ef8bb33e
Seth Fitzsimmons <seth@mojodna.net>
2016-03-08 07:34:16 -0800

    Mapnik is no longer a tl dependency

--- kickstart/scripts/tl-deploy.sh
+++ kickstart/scripts/tl-deploy.sh
@@ -5,12 +5,8 @@ deploy_tl_ubuntu() {
   npm install -g tl
   local prefix=`npm prefix -g`
 
-  # move mapnik up a level in the tree (virtual tree w/ npm@3, but still...)
-  (cd $prefix/lib/node_modules/tl && find node_modules -type d -name mapnik -exec rm -rf {} \;)
-  (cd $prefix/lib/node_modules/tl && npm install mapnik@3.5.2)
-
   # install useful modules for tile rendering
-  (cd $prefix/lib/node_modules/tl && npm install mbtiles tilelive-http tilejson tilelive-mapnik tilelive-blend)
+  (cd $prefix/lib/node_modules/tl && npm install mapnik@3.5.2 mbtiles tilelive-http tilejson tilelive-mapnik tilelive-blend)
 }
 
 deploy tl
