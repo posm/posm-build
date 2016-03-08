9d1fd956b05a335b68520bce532da91012a05dc5
Seth Fitzsimmons <seth@mojodna.net>
2016-03-07 21:00:36 -0800

    Silence warning

--- kickstart/scripts/tessera-deploy.sh
+++ kickstart/scripts/tessera-deploy.sh
@@ -8,7 +8,7 @@ deploy_tessera_ubuntu() {
   (cd $prefix/lib/node_modules/tessera && npm install mbtiles tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource tilelive-file tilelive-http tilelive-mapbox tilejson tilelive-vector tilelive-blend)
 
   # configure
-  mkdir /etc/tessera.conf.d
+  mkdir -p /etc/tessera.conf.d
   expand etc/posm-carto.json /etc/tessera.conf.d/posm-carto.json
   expand etc/openstreetmap-carto.json /etc/tessera.conf.d/openstreetmap-carto.json
 
