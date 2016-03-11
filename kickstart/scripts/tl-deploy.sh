575e86d517e36eb8a2aef9a4dc8ba0e5a04634a2
Seth Fitzsimmons <seth@mojodna.net>
2016-03-11 18:19:16 -0500

    Clean up varying mapnik versions after
    Also makes additional modules available to tl

--- kickstart/scripts/tl-deploy.sh
+++ kickstart/scripts/tl-deploy.sh
@@ -5,7 +5,10 @@ deploy_tl_ubuntu() {
   local prefix=`npm prefix -g`
 
   # install useful modules for tile rendering
-  (cd $prefix/lib/node_modules/tl && npm install mapnik mbtiles tilelive-http tilejson tilelive-mapnik tilelive-blend)
+  (cd $prefix/lib/node_modules/tl && npm install mbtiles tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource tilelive-file tilelive-http tilelive-mapbox tilejson tilelive-vector tilelive-blend)
+  # move mapnik up a level in the tree (virtual tree w/ npm@3, but still...)
+  (cd $prefix/lib/node_modules/tl && find node_modules -type d -name mapnik -exec rm -rf {} \;)
+  (cd $prefix/lib/node_modules/tl && npm install mapnik)
 }
 
 deploy tl
