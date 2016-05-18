3054d8cc8c86e9abf20ded7147f0040856ee6b10
Daniel Baah <dbaah@spatialdev.com>
2016-05-18 15:36:48 -0700

    use /opt/data/backups directory

--- kickstart/scripts/samba-deploy.sh
+++ kickstart/scripts/samba-deploy.sh
@@ -11,11 +11,15 @@ deploy_samba_ubuntu() {
   chown nobody:nogroup /opt/data/fieldpapers
   chmod 777 /opt/data/fieldpapers
 
-  mkdir -p /opt/data/omk
+  mkdir -p /opt/data/backups/omk
   chown nobody:nogroup /opt/data/omk
   chmod 777 /opt/data/omk
 
-  mkdir -p /opt/data/osm
+  mkdir -p /opt/data/backups/osm
+  chown nobody:nogroup /opt/data/osm
+  chmod 777 /opt/data/osm
+
+  mkdir -p /opt/data/backups/fieldpapers
   chown nobody:nogroup /opt/data/osm
   chmod 777 /opt/data/osm
 
