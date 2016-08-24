e8baa30a75211b014e53feee3702b620d4d142d8
Seth Fitzsimmons <seth@mojodna.net>
2016-08-24 11:24:20 -0700

    Skip interfaces if undefined

--- kickstart/scripts/wifi-deploy.sh
+++ kickstart/scripts/wifi-deploy.sh
@@ -24,9 +24,13 @@ deploy_wifi_ubuntu() {
     rng-tools
 
   expand etc/hosts "/etc/hosts"
-  for f in $(ls etc/network/interfaces.d); do
-    expand etc/network/interfaces.d/$f "/etc/network/interfaces.d/$f"
-  done
+
+  # configure network interfaces
+  expand etc/network/interfaces.d/usb0.cfg "/etc/network/interfaces.d/usb0.cfg"
+  test "$posm_lan_netif" != "" && expand etc/network/interfaces.d/lan.cfg "/etc/network/interfaces.d/${posm_lan_netif}.cfg"
+  test "$posm_wan_netif" != "" && expand etc/network/interfaces.d/wan.cfg "/etc/network/interfaces.d/${posm_wan_netif}.cfg"
+  test "$posm_wlan_netif" != "" && expand etc/network/interfaces.d/wlan.cfg "/etc/network/interfaces.d/${posm_wlan_netif}.cfg"
+
   expand etc/hostapd.conf "/etc/hostapd/hostapd.conf"
   expand etc/dnsmasq-posm.conf "/etc/dnsmasq.d/50-posm.conf"
   expand etc/dnsmasq-default "/etc/default/dnsmasq"
