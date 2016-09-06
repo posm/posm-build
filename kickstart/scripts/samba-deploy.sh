833ecae322380544f0543fd35f714a5e9f86091f
Seth Fitzsimmons <seth@mojodna.net>
2016-09-06 11:50:48 -0700

    Samba shares for OpenDroneMap
    Fixes AmericanRedCross/posm#200
    Fixes AmericanRedCross/posm#207
    Refs AmericanRedCross/posm#213

--- kickstart/scripts/samba-deploy.sh
+++ kickstart/scripts/samba-deploy.sh
@@ -3,14 +3,32 @@
 deploy_samba_ubuntu() {
   apt-get install --no-install-recommends -y samba
 
+  # writable spot to share files with other POSM users
   mkdir -p /opt/data/public
   chown nobody:nogroup /opt/data/public
   chmod 777 /opt/data/public
 
+  # writable spot to put Field Papers snapshots
   mkdir -p /opt/data/fieldpapers
   chown nobody:nogroup /opt/data/fieldpapers
   chmod 777 /opt/data/fieldpapers
 
+  # writable spot to put input images for ODM
+  mkdir -p /opt/data/odm-input
+  chown nobody:nogroup /opt/data/odm-input
+  chmod 777 /opt/data/odm-input
+
+  # writable spot to put GeoTIFFs for tiling (and where ODM outputs are copied)
+  mkdir -p /opt/data/aerial-imagery
+  chown nobody:nogroup /opt/data/aerial-imagery
+  chmod 777 /opt/data/aerial-imagery
+
+  # read-only spot to copy MBTiles archives from
+  mkdir -p /opt/data/mbtiles
+  chown nobody:nogroup /opt/data/mbtiles
+  chmod 644 /opt/data/mbtiles
+
+  # read-only spot to copy backups from
   mkdir -p /opt/data/backups
   chmod 644 /opt/data/backups
 
