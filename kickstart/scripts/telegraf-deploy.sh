b9b3e5cf8e4118d2f5f72f88a506e52463a46419
Seth Fitzsimmons <seth@mojodna.net>
2016-09-08 07:37:44 -0700

    Upgrade InfluxDB + Telegraf to 1.0.0

--- kickstart/scripts/telegraf-deploy.sh
+++ kickstart/scripts/telegraf-deploy.sh
@@ -2,8 +2,8 @@
 
 deploy_telegraf_ubuntu() {
   mkdir -p "${BOOTSTRAP_HOME}/sources"
-  wget -q -N -P "${BOOTSTRAP_HOME}/sources" https://dl.influxdata.com/telegraf/releases/telegraf_0.13.2_amd64.deb
-  dpkg -i "${BOOTSTRAP_HOME}/sources/telegraf_0.13.2_amd64.deb"
+  wget -q -N -P "${BOOTSTRAP_HOME}/sources" https://dl.influxdata.com/telegraf/releases/telegraf_1.0.0_amd64.deb
+  dpkg -i "${BOOTSTRAP_HOME}/sources/telegraf_1.0.0_amd64.deb"
 
   for f in etc/telegraf/telegraf.d/*; do
     expand "etc/telegraf/telegraf.d/$f" "/etc/telegraf/telegraf.d/$f"
