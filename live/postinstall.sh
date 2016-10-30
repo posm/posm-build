43dbe9c632d0921f2602d3b370e42d0ee0b58bbb
Seth Fitzsimmons <seth@mojodna.net>
2016-10-30 16:55:08 -0700

    Makefile-driven approach

---
+++ live/postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chroot /target mount -t proc proc /proc
+chroot /target mount -t sysfs sysfs /sys
+chroot /target mount -t securityfs securityfs /sys/kernel/security
+mount --bind /dev /target/dev
+mount --bind /run /target/run
+
+awk '{print $1}' /cdrom/casper/filesystem-remove.manifest | xargs chroot /target apt-get purge -y
+chroot /target apt-get autoremove
+# TODO split wifi into package installation and configuration
+chroot /target /root/posm-build/kickstart/scripts/bootstrap.sh wifi captive
