7643b2fc58a792f4eb72bd3f1362b8122360f2fc
Seth Fitzsimmons <seth@mojodna.net>
2016-11-08 14:07:30 -0800

    Split wifi deps from hotspot config

--- live/postinstall.sh
+++ live/postinstall.sh
@@ -8,7 +8,6 @@ mount --bind /run /target/run
 
 awk '{print $1}' /cdrom/casper/filesystem-remove.manifest | xargs chroot /target apt-get purge -y
 chroot /target apt-get autoremove
-# TODO split wifi into package installation and configuration
-chroot /target /root/posm-build/kickstart/scripts/bootstrap.sh wifi captive
+chroot /target /root/posm-build/kickstart/scripts/bootstrap.sh hotspot captive
 chroot /target ln -s /root/posm-build/kickstart/scripts /root/scripts
 chroot /target ln -s /root/posm-build/kickstart/etc /root/etc
