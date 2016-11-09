a3d75e2097cebac1e059c32c745c7bb2e567b3c1
Seth Fitzsimmons <seth@mojodna.net>
2016-11-08 18:38:37 -0800

    Sync with ODM/Imagery API locations

--- kickstart/scripts/samba-deploy.sh
+++ kickstart/scripts/samba-deploy.sh
@@ -13,21 +13,6 @@ deploy_samba_ubuntu() {
   chown nobody:nogroup /opt/data/fieldpapers
   chmod 777 /opt/data/fieldpapers
 
-  # writable spot to put input images for ODM
-  mkdir -p /opt/data/odm-input
-  chown nobody:nogroup /opt/data/odm-input
-  chmod 777 /opt/data/odm-input
-
-  # writable spot to put GeoTIFFs for tiling (and where ODM outputs are copied)
-  mkdir -p /opt/data/aerial-imagery
-  chown nobody:nogroup /opt/data/aerial-imagery
-  chmod 777 /opt/data/aerial-imagery
-
-  # read-only spot to copy MBTiles archives from
-  mkdir -p /opt/data/mbtiles
-  chown nobody:nogroup /opt/data/mbtiles
-  chmod 644 /opt/data/mbtiles
-
   # read-only spot to copy backups from
   mkdir -p /opt/data/backups
   chmod 644 /opt/data/backups
