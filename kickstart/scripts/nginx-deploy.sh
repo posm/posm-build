3b44a924e54428ad9e6dc464a92c9291ee0f7072
Seth Fitzsimmons <seth@mojodna.net>
2017-04-26 21:11:49 -0700

    Configurable Basic Auth
    To enable, set `auth=<anything>` in `settings`.
    
    Fixes posm/posm#283

--- kickstart/scripts/nginx-deploy.sh
+++ kickstart/scripts/nginx-deploy.sh
@@ -3,10 +3,14 @@
 dst=/opt/posm-www
 
 deploy_nginx_ubuntu() {
-  apt-get install --no-install-recommends -y nginx make
+  apt-get install --no-install-recommends -y nginx make apache2-utils
   expand etc/nginx-posm.conf /etc/nginx/sites-available/posm
   rm -f /etc/nginx/sites-enabled/default
   ln -sf ../sites-available/posm /etc/nginx/sites-enabled/00-posm
+
+  # make auth credentials available in case auth is enabled
+  htpasswd -bc /etc/nginx/htpasswd $auth_user $auth_password
+
   service nginx restart
 
   mkdir -p "$dst"
