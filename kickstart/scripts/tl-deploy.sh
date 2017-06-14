4361a6363e67fb9ff6dae49add0c8354be034811
Seth Fitzsimmons <seth@mojodna.net>
2017-06-13 19:39:47 -0700

    Newer glibc is required

--- kickstart/scripts/tl-deploy.sh
+++ kickstart/scripts/tl-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 deploy_tl_ubuntu() {
+  add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  apt-get update
+  apt-get install -y libstdc++6
+
   npm install -g mapnik@~3.6.1 @mapbox/mbtiles @mapbox/tilelive \
     @mapbox/tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource \
     tilelive-file tilelive-http tilelive-mapbox @mapbox/tilejson \
