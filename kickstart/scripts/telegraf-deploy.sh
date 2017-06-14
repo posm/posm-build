e8f295445d8f9df708991d965fcf3fecf02a1255
Seth Fitzsimmons <seth@mojodna.net>
2017-06-13 18:30:09 -0700

    Really fix telegraf filename

--- kickstart/scripts/telegraf-deploy.sh
+++ kickstart/scripts/telegraf-deploy.sh
@@ -3,7 +3,7 @@
 deploy_telegraf_ubuntu() {
   mkdir -p "${BOOTSTRAP_HOME}/sources"
   wget -q -N -P "${BOOTSTRAP_HOME}/sources" https://dl.influxdata.com/telegraf/releases/telegraf_1.3.1-1_amd64.deb
-  dpkg -i "${BOOTSTRAP_HOME}/sources/telegraf_1.3.1_amd64.deb"
+  dpkg -i "${BOOTSTRAP_HOME}/sources/telegraf_1.3.1-1_amd64.deb"
 
   for f in etc/telegraf/telegraf.d/*; do
     expand "etc/telegraf/telegraf.d/$f" "/etc/telegraf/telegraf.d/$f"
