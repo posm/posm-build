4d86755b5789bc31df086d8e0f1117b224ff49a6
Seth Fitzsimmons <seth@mojodna.net>
2017-06-13 19:25:17 -0700

    Match version updates throughout the tilelive ecosystem

--- kickstart/scripts/tessera-deploy.sh
+++ kickstart/scripts/tessera-deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 deploy_tessera_ubuntu() {
-  add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  apt-get update
-  apt-get install -y libstdc++6
-
-  npm install -g mapnik@~3.5.14 mbtiles tilelive tilelive-mapnik tilelive-carto tilelive-tmstyle \
-    tilelive-tmsource tilelive-file tilelive-http tilelive-mapbox tilejson tilelive-vector \
-    tilelive-blend tessera @posm/posm-imagery-updater
+  npm install -g mapnik@~3.6.1 @mapbox/mbtiles @mapbox/tilelive \
+    @mapbox/tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource \
+    tilelive-file tilelive-http tilelive-mapbox @mapbox/tilejson \
+    @mapbox/tilelive-vector tilelive-blend tessera @posm/posm-imagery-updater
 
   # configure
   mkdir -p /etc/tessera.conf.d
