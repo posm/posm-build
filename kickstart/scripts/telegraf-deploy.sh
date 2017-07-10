ac80cd244290e6c1c614c28d3b2ea090b5b78873
Seth Fitzsimmons <seth@mojodna.net>
2017-07-10 16:12:01 -0700

    Correctly link telegraf configs

--- kickstart/scripts/telegraf-deploy.sh
+++ kickstart/scripts/telegraf-deploy.sh
@@ -5,8 +5,8 @@ deploy_telegraf_ubuntu() {
   wget -q -N -P "${BOOTSTRAP_HOME}/sources" https://dl.influxdata.com/telegraf/releases/telegraf_1.3.1-1_amd64.deb
   dpkg -i "${BOOTSTRAP_HOME}/sources/telegraf_1.3.1-1_amd64.deb"
 
-  for f in etc/telegraf/telegraf.d/*; do
-    expand "etc/telegraf/telegraf.d/$f" "/etc/telegraf/telegraf.d/$f"
+  for f in ${BOOTSTRAP_HOME}/etc/telegraf/telegraf.d/*.conf; do
+    expand "etc/telegraf/telegraf.d/$(basename $f)" "/etc/telegraf/telegraf.d/$(basename $f)"
   done
 
   service telegraf restart
