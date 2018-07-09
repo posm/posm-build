399129fbbd3580192b05159da865d39b04fe521a
Seth Fitzsimmons <seth@mojodna.net>
2018-07-09 23:23:38 +0000

    Point to branch w/ Ruby-2.5 + PostgreSQL 10 fixes

--- kickstart/scripts/osm-deploy.sh
+++ kickstart/scripts/osm-deploy.sh
@@ -60,7 +60,7 @@ deploy_osm_rails() {
   npm install -g svgo
 
   # install OSM WEB
-  from_github "https://github.com/AmericanRedCross/openstreetmap-website" "$dst/osm-web" "posm-v0.7.0"
+  from_github "https://github.com/AmericanRedCross/openstreetmap-website" "$dst/osm-web" "posm-v0.8.0"
   chown -R osm:osm "$dst/osm-web"
 
   # service-friendly serving + logging
