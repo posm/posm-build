6719d3baf458c775407cd1fe71e1592ebc81b6b9
Seth Fitzsimmons <seth@mojodna.net>
2018-07-10 21:41:24 +0000

    Bionic compatibility with posm-carto

--- kickstart/scripts/carto-deploy.sh
+++ kickstart/scripts/carto-deploy.sh
@@ -6,7 +6,7 @@ dst="/opt/$carto_user"
 tessera_config_dir=/etc/tessera.conf.d
 
 deploy_carto_ubuntu() {
-  apt-get install --no-install-recommends -y unzip make postgresql-contrib-9.6
+  apt-get install --no-install-recommends -y unzip make postgresql-contrib-10
   useradd -c 'OSM/GIS User' -d "$dst" -m -r -s /bin/bash -U "$carto_user"
   mkdir -p $tessera_config_dir
   chown $carto_user:$carto_user $tessera_config_dir
@@ -56,6 +56,7 @@ deploy_carto_posm() {
   chown "$carto_user:$carto_user" /opt/data/osm
   chown "$carto_user:$carto_user" /opt/data/osm/expiry
 
+  sudo -u $carto_user rm -f /opt/data/osm/configuration.txt
   sudo -u $carto_user osmosis --read-replication-interval-init workingDirectory=/opt/data/osm
   sudo -u $carto_user sed -Ei 's!^baseUrl\s?=.*$!baseUrl=file:///opt/data/osm/replication/minute!' /opt/data/osm/configuration.txt
   sudo -u $carto_user sed -Ei 's!^maxInterval\s?=.*$!maxInterval=0!' /opt/data/osm/configuration.txt
