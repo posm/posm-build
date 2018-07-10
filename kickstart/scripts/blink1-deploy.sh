d7e21f1768e3517ed9f2abad8def5c09a671184c
Seth Fitzsimmons <seth@mojodna.net>
2018-07-10 22:15:14 +0000

    Bionic compatibility with blink(1)

--- kickstart/scripts/blink1-deploy.sh
+++ kickstart/scripts/blink1-deploy.sh
@@ -2,12 +2,13 @@
 
 deploy_blink1_ubuntu() {
   TMPFILE=$(mktemp --suffix .zip)
-  curl -L https://github.com/todbot/blink1/releases/download/v1.96/blink1-tool-v1.96-linux-x86_64.zip -o $TMPFILE
+  curl -L https://github.com/todbot/blink1/releases/download/v1.98a/blink1-tool-v1.98a-linux-x86_64.zip -o $TMPFILE
   unzip -d /usr/local/bin $TMPFILE
   chmod +x /usr/local/bin/blink1-tool
   rm $TMPFILE
 
-  expand etc/init/blink1.conf /etc/init/blink1.conf
+  expand etc/systemd/system/blink1.service /etc/systemd/system/blink1.service
+  systemctl enable blink1
 }
 
 deploy blink1
