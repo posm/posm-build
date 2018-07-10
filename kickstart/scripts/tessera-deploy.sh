0c98a2374a6bf20dc344791343e6574814c54b80
Seth Fitzsimmons <seth@mojodna.net>
2018-07-10 21:53:21 +0000

    Bionic compatibility with tessera

--- kickstart/scripts/tessera-deploy.sh
+++ kickstart/scripts/tessera-deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 deploy_tessera_ubuntu() {
-  add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  apt-get update
-  apt-get install -y libstdc++6
-
-  npm install -g mapnik@~3.7.2 @mapbox/mbtiles @mapbox/tilelive \
+  npm install --unsafe-perm -g mapnik@~3.7.2 @mapbox/mbtiles @mapbox/tilelive \
     @mapbox/tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource \
     tilelive-file tilelive-http tilelive-mapbox @mapbox/tilejson \
     @mapbox/tilelive-vector tilelive-blend tessera @posm/posm-imagery-updater
@@ -13,7 +9,8 @@ deploy_tessera_ubuntu() {
   # configure
   mkdir -p /etc/tessera.conf.d
 
-  expand etc/tessera.upstart /etc/init/tessera.conf
+  expand etc/systemd/system/tessera.service.hbs /etc/systemd/system/tessera.service
+  systemctl enable tessera
 
   service tessera restart
 
