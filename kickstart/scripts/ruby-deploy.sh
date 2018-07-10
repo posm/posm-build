ff677d814d1fc0704d3a9fba828914e92711d586
Seth Fitzsimmons <seth@mojodna.net>
2018-07-10 18:11:10 +0000

    Revert "Remove ruby-from-source paths"
    This reverts commit 7f3ab2e96bdce6771a765fcc4352cbabd22b881e.

--- kickstart/scripts/ruby-deploy.sh
+++ kickstart/scripts/ruby-deploy.sh
@@ -2,13 +2,44 @@
 
 ruby_ver="${ruby_ver:-2.4}"
 
-deploy_ruby_ubuntu() {
-  apt-add-repository -y ppa:brightbox/ruby-ng
-  apt-get update
-  apt-get install --no-install-recommends -y \
-    ruby${ruby_ver} ruby${ruby_ver}-dev
+fromsource=no
+ruby_prefix="${ruby_prefix:-/opt/rbenv}"
 
+deploy_ruby_ubuntu() {
+  if [ x"$fromsource" = x"yes" ]; then
+    apt-get install --no-install-recommends -y \
+      build-essential \
+      libcurl4-openssl-dev \
+      libffi-dev \
+      libreadline-dev \
+      libsqlite3-dev \
+      libssl-dev \
+      libxml2-dev \
+      libxslt1-dev \
+      libyaml-dev \
+      sqlite3 \
+      zlib1g-dev
+    deploy_ruby_fromsource
+  else
+    apt-add-repository -y ppa:brightbox/ruby-ng
+    apt-get update
+    apt-get install --no-install-recommends -y \
+      ruby${ruby_ver} ruby${ruby_ver}-dev
+  fi
   deploy_ruby_generic
+}
+
+deploy_ruby_fromsource() {
+  from_github https://github.com/rbenv/rbenv "$ruby_prefix"
+
+  from_github https://github.com/sstephenson/ruby-build "$ruby_prefix/plugins/ruby-build"
+
+  export PATH="$PATH:$ruby_prefix/bin:$ruby_prefix/plugins/ruby-build/bin"
+  export RBENV_ROOT="$ruby_prefix"
+  eval "$(rbenv init -)"
+
+  rbenv install -v $ruby_ver
+  rbenv global $ruby_ver
 }
 
 deploy_ruby_generic() {
