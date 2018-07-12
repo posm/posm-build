3bbb776420118fcf1d851171f3482b845788e99b
Seth Fitzsimmons <seth@mojodna.net>
2018-07-12 12:44:38 -0700

    Words

--- kickstart/etc/apply-updates.sh
+++ kickstart/etc/apply-updates.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 PATH=/usr/local/bin:/usr/sbin:/usr/bin:/bin
 
-# --rri will automatically fetch the latest state.txt; it will initialized when osm2pgsql fully imports an extract
+# --rri will automatically fetch the latest state.txt; it will be initialized when osm2pgsql fully imports an extract
 if [ -f /opt/data/osm/state.txt ]; then
   db=$(jq -r .osm_carto_pg_dbname /etc/posm.json)
   timestamp=$(date -u +\%Y\%m\%d-\%H\%M)
