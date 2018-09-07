a8b3505503194153419a6b66a1bfb10519867d44
Seth Fitzsimmons <seth@mojodna.net>
2018-09-07 11:02:35 -0700

    Pin to specific minor versions of node modules

--- kickstart/scripts/tessera-deploy.sh
+++ kickstart/scripts/tessera-deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
 deploy_tessera_ubuntu() {
-  npm install --unsafe-perm -g mapnik@~3.7.2 @mapbox/mbtiles @mapbox/tilelive \
-    @mapbox/tilelive-mapnik tilelive-carto tilelive-tmstyle tilelive-tmsource \
-    tilelive-file tilelive-http tilelive-mapbox @mapbox/tilejson \
-    @mapbox/tilelive-vector tilelive-blend tessera @posm/posm-imagery-updater
+  npm install --unsafe-perm -g \
+    mapnik@~3.7.2 \
+    @mapbox/mbtiles@~0.10.0 \
+    @mapbox/tilelive@~6.0.0 \
+    @mapbox/tilelive-mapnik@~1.0.0 \
+    tilelive-carto@~0.8.0 \
+    tilelive-tmstyle@~0.8.0 \
+    tilelive-tmsource@~0.8.2 \
+    tilelive-file@~0.0.3 \
+    tilelive-http@~0.14.0 \
+    tilelive-mapbox@~0.5.0 \
+    @mapbox/tilejson@~1.1.0 \
+    @mapbox/tilelive-vector@~4.2.0 \
+    tilelive-blend@~0.5.1 \
+    tessera@~0.13.1 \
+    @posm/posm-imagery-updater
 
   # configure
   mkdir -p /etc/tessera.conf.d
