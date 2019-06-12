148119905331a74ede0d858e9a80db63c40b5694
Seth Fitzsimmons <seth@mojodna.net>
2019-06-11 19:43:27 -0700

    Use host networking for ODM-related containers
    This will allow them to connect to link local addresses without having
    to restart Docker and containers.
    
    This also removes PostgreSQL as a WebODM dependency.

--- kickstart/scripts/redis-deploy.sh
+++ kickstart/scripts/redis-deploy.sh
@@ -2,7 +2,7 @@
 
 deploy_redis_ubuntu() {
   docker pull redis
-  docker create --name redis redis
+  docker create --name redis --network host redis
 
   expand etc/systemd/system/redis.service.hbs /etc/systemd/system/redis.service
   systemctl enable --now redis
--- kickstart/scripts/webodm-deploy.sh
+++ kickstart/scripts/webodm-deploy.sh
@@ -5,12 +5,6 @@ deploy_webodm_ubuntu() {
   mkdir -p /opt/data/webodm/project
   chown -R webodm:webodm /opt/data/webodm
 
-  echo -e "${webodm_pg_pass}\n${webodm_pg_pass}" | su - postgres -c "createuser --no-superuser --no-createdb --no-createrole --pwprompt '$webodm_pg_owner'"
-  su - postgres -c "createdb --owner='$webodm_pg_owner' '$webodm_pg_dbname'"
-  su - postgres -c "psql --dbname='$webodm_pg_dbname' --command='CREATE EXTENSION postgis'"
-  su - postgres -c "psql --dbname='$webodm_pg_dbname' --command='ALTER DATABASE $webodm_pg_dbname SET postgis.gdal_enabled_drivers TO 'GTiff';'"
-  su - postgres -c "psql --dbname='$webodm_pg_dbname' --command='ALTER DATABASE $webodm_pg_dbname SET postgis.enable_outdb_rasters TO True;'"
-
   docker pull opendronemap/webodm_webapp@${webodm_webapp_digest}
 
   expand etc/webodm.py.hbs /etc/webodm.py
@@ -35,7 +29,7 @@ deploy_webodm_ubuntu() {
 
   docker exec webodm-web.service /webodm/wait-for-it.sh -t 0 localhost:8000
   docker exec webodm-web.service python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$webodm_user', '', '$webodm_password')"
-  docker exec webodm-web.service python manage.py shell -c "from nodeodm.models import ProcessingNode; ProcessingNode.objects.update_or_create(hostname='clusterodm.service', defaults={'hostname': 'clusterodm.service', 'port': 3000, 'label': 'Cluster'})"
+  docker exec webodm-web.service python manage.py shell -c "from nodeodm.models import ProcessingNode; ProcessingNode.objects.update_or_create(hostname='localhost', defaults={'hostname': 'localhost', 'port': 3000, 'label': 'Cluster'})"
 
   # add the nginx config for the WebODM virtualhost
   expand etc/nginx-webodm.conf /etc/nginx/sites-available/webodm
