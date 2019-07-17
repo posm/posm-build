4e9487454a2c1a0684e7a2ecb6cd51c2af87dbcf
Seth Fitzsimmons <seth@mojodna.net>
2019-07-17 16:46:22 -0700

    ClusterODM cares about indentation

--- kickstart/usr/local/bin/register-nodeodm-worker.sh
+++ kickstart/usr/local/bin/register-nodeodm-worker.sh
@@ -7,6 +7,6 @@ hostname=${1:-$(hostname -I | awk '{print $1}')}
 
 for i in {1..5}; do
   cat << EOF | nc -N ${posm_hostname}.local 28080 && break || sleep 15
-  NODE ADD ${hostname} 3001
+NODE ADD ${hostname} 3001
 EOF
 done
