364b4d5fa69dd563b3bf429c4696a711420ab0c2
Seth Fitzsimmons <seth@mojodna.net>
2019-07-17 16:22:42 -0700

    Attempt to register 5 times, with 15s breaks

--- kickstart/usr/local/bin/register-nodeodm-worker.sh
+++ kickstart/usr/local/bin/register-nodeodm-worker.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 posm_hostname=$(jq -r .posm_hostname /etc/posm.json)
 hostname=${1:-$(hostname -I | awk '{print $1}')}
 
-cat << EOF | nc -N ${posm_hostname}.local 28080
-NODE ADD ${hostname} 3001
+for i in {1..5}; do
+  cat << EOF | nc -N ${posm_hostname}.local 28080 && break || sleep 15
+  NODE ADD ${hostname} 3001
 EOF
+done
