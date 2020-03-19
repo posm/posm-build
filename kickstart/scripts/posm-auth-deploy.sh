ade38acc821133ac06bf29db101ee093af428bbe
bewakes <bewakepandey@gmail.com>
2020-03-19 17:33:15 +0545

    Fix typos and issues

--- kickstart/scripts/posm-auth-deploy.sh
+++ kickstart/scripts/posm-auth-deploy.sh
@@ -22,16 +22,16 @@ deploy_posm_auth_ubuntu() {
 
     # Expand and copy nginx confs and include files
     expand etc/nginx-posm-auth.conf /etc/nginx/sites-available/posm-auth
-    expand etc/nginx-posm-auth.include /etc/nginx/sites-available/posm-auth.include
+    expand etc/nginx-posm-auth.common /etc/nginx/snippets/posm-auth.common
     ln -s -f ../sites-available/posm-auth.include /etc/nginx/sites-enabled/
     ln -s -f ../sites-available/posm-auth /etc/nginx/sites-enabled/
 
     # Copy html and static files for error pages
     mkdir -p $dst/assets/images
-    cp etc/posm_auth/assets/images/* $dst/assets/images/
+    cp etc/posm-auth/assets/images/* $dst/assets/images/
     # Expand the html files which contain {{auth_base_url}}
-    expand etc/posm-auth/assets/403.html $dst/assets/
-    expand etc/posm-auth/assets/404.html $dst/assets/
+    expand etc/posm-auth/assets/403.html $dst/assets/403.html
+    expand etc/posm-auth/assets/404.html $dst/assets/404.html
 
     # copy systemd unit into place, replacing template variables in the process
     expand etc/systemd/system/posm-auth.service.hbs /etc/systemd/system/posm-auth.service
