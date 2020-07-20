4c564c1572e6df90cd21d9af51d104a3cadea638
bewakes <bewakepandey@gmail.com>
2020-07-20 10:42:03 +0545

    Add replay-tool-client build

--- kickstart/scripts/replay-tool-deploy.sh
+++ kickstart/scripts/replay-tool-deploy.sh
@@ -4,26 +4,16 @@ dst=/opt/replay-tool
 server_path=$dst/posm-replay-server
 client_path=$dst/posm-replay-client
 
-# Ubuntu-specific deployment
-deploy_replay_tool_ubuntu() {
+deploy_server() {
     mkdir -p "$dst"
-    mkdir -p "$client_path"
     chmod 755 "$dst"
-
-    rm -rf "$client_path/*" 2> /dev/null
 
     # Install docker compose
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
-    # Clone server and client
+    # Clone server
     git clone https://github.com/posm/posm-replay-server "$dst/posm-replay-server"
-
-    #  TODO: Use client release
-    wget https://github.com/posm/posm-replay-client/releases/download/0.1/build.zip
-    unzip build.zip -d "$client_path"
-    chown www-data:www-data "$client_path" -R
-    rm bulid.zip
 
     # Copy env vars for server
     cp "$dst/posm-replay-server/env_sample" "$server_path/.env"
@@ -41,4 +31,29 @@ deploy_replay_tool_ubuntu() {
     systemctl start replay-tool.service
 }
 
-deploy replay_tool
+deploy_client() {
+    mkdir -p "$dst"
+    mkdir -p "$client_path"
+    chmod 755 "$dst"
+
+    rm -rf "$client_path/*" 2> /dev/null
+
+    # Clone client
+    git clone https://github.com/posm/posm-replay-client "$client_path"
+    Copy environment variables
+    cp "$dst/posm-replay-client/env_sample" "$client_path/.env"
+
+    cd $client_path && docker run --rm -it -v $(pwd):/code node:8.16.0-alpine sh -c 'apk add git && cd code && yarn install && yarn build'
+
+    chown www-data:www-data "$client_path" -R
+}
+
+# Ubuntu-specific deployment
+deploy_replay_tool_ubuntu() {
+    deploy_client
+    deploy_server
+}
+
+deploy_replay_tool_ubuntu
+
+# deploy replay_tool
