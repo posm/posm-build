9c3812bb905b99d8e6cf26bf189fd69f8e44558a
bewakes <bewakepandey@gmail.com>
2020-07-23 16:47:04 +0545

    Update replay client deploy

--- kickstart/scripts/replay-tool-deploy.sh
+++ kickstart/scripts/replay-tool-deploy.sh
@@ -5,18 +5,18 @@ server_path=$dst/posm-replay-server
 client_path=$dst/posm-replay-client
 
 deploy_server() {
-    mkdir -p "$dst"
-    chmod 755 "$dst"
+    mkdir -p $dst
+    chmod 755 $dst
 
     # Install docker compose
     sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     # Clone server
-    git clone https://github.com/posm/posm-replay-server "$dst/posm-replay-server"
+    git clone https://github.com/posm/posm-replay-server $dst/posm-replay-server
 
     # Copy env vars for server
-    cp "$dst/posm-replay-server/env_sample" "$server_path/.env"
+    cp $dst/posm-replay-server/env_sample $server_path/.env
 
     expand etc/nginx-replay-tool.conf /etc/nginx/sites-available/replay-tool
     ln -s -f ../sites-available/replay-tool /etc/nginx/sites-enabled/
@@ -32,17 +32,31 @@ deploy_server() {
 }
 
 deploy_client() {
-    mkdir -p "$dst"
-    mkdir -p "$client_path"
-    chmod 755 "$dst"
+    client_path=`pwd`/testing
+    vendor_path=$client_path/src/vendor
 
-    rm -rf "$client_path/*" 2> /dev/null
+    echo Creating/Clearing directories...
+    mkdir -p $dst
+    mkdir -p $client_path
+    chmod 755 $dst
+
+    rm -rf $client_path 2> /dev/null
 
     # Clone client
-    git clone https://github.com/posm/posm-replay-client "$client_path"
-    Copy environment variables
-    cp "$dst/posm-replay-client/env_sample" "$client_path/.env"
+    echo Cloning posm-replay-client...
+    ls $client_path
+    git clone https://github.com/posm/posm-replay-client $client_path
+    # Clone other dependencies
+    echo Cloning other depencies...
+    mkdir -p $vendor_path
+    cd $vendor_path && git clone https://github.com/posm/react-store
+    cd $vendor_path && git clone https://github.com/posm/re-map
 
+    # Copy environment variables
+    echo Copying environment variables...
+    cp $client_path/env_sample $client_path/.env
+
+    echo Building client...
     cd $client_path && docker run --rm -it -v $(pwd):/code node:8.16.0-alpine sh -c 'apk add git && cd code && yarn install && yarn build'
 
     chown www-data:www-data "$client_path" -R
@@ -55,5 +69,3 @@ deploy_replay_tool_ubuntu() {
 }
 
 deploy_replay_tool_ubuntu
-
-# deploy replay_tool
